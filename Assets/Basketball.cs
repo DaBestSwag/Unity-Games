79a7cef8df9818ae9e257ceab24b113c2f5394b7
DaBestSwag <evanhaacke@gmail.com>
2015-07-05 16:19:50 -0400

    Added Sound To Basketball
    VERY annoying

---
+++ Assets/Basketball.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Basketball : MonoBehaviour {
+
+	public AudioClip boink;
+	public AudioSource myAudio;
+	public static int score;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collider){
+		myAudio.PlayOneShot(boink);
+		score++;
+		Debug.Log (score);
+
+	}
+}
